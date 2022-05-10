0bf5c7f3779d744024c354dbee6778c7ae9510f0
Kai Anter <kai.anter@web.de>
2022-05-10 21:21:55 +0200

    Initial Commit

---
+++ src/main/java/com/example/tatort4j/Tatort4jApplication.java
@@ -0,0 +1,13 @@
+package com.example.tatort4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tatort4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tatort4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tatort4j/Tatort4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tatort4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tatort4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
